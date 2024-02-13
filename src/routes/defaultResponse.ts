12e91d3afc9a34b302e17fec802483b967bb44c3
c-bertran <cbertran@student.42.fr>
2024-02-13 01:08:50 +0100

    Patch error name

--- src/routes/defaultResponse.ts
+++ src/routes/defaultResponse.ts
@@ -2,8 +2,8 @@ import { STATUS_CODES } from 'http';
 
 export default (code = 200): any => {
 	return {
-		info: 'Sybillin API',
-		copyright: `Sybillin API 2023 - ${new Date().getFullYear()}`,
+		info: 'Sybylin API',
+		copyright: `Sybylin API 2023 - ${new Date().getFullYear()}`,
 		code: code,
 		status: STATUS_CODES[code]
 	};
